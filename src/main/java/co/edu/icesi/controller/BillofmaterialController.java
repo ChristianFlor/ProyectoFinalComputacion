3f050a770ad51be909cba5e734792e0b0714dd18
andreanr19 <andrea.nunez1@correo.icesi.edu.co>
2021-12-15 14:17:38 -0500

    Added Billofmaterial controller

--- src/main/java/co/edu/icesi/controller/BillofmaterialController.java
+++ src/main/java/co/edu/icesi/controller/BillofmaterialController.java
@@ -14,43 +14,33 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import co.edu.icesi.businessdelegate.BusinessDelegate;
 import co.edu.icesi.model.Add;
+import co.edu.icesi.model.Billofmaterial;
 import co.edu.icesi.model.Productreview;
-import co.edu.icesi.model.Transactionhistory;
-import co.edu.icesi.services.ProductService;
-import co.edu.icesi.services.ProductreviewService;
 
 @Controller
-@RequestMapping("/product-reviews")
-public class ProductreviewController {
+@RequestMapping("/billofmaterials")
+public class BillofmaterialController {
 
 	@Autowired
 	BusinessDelegate delegate;
-
-	private ProductService productService;
-	private ProductreviewService productreviewService;
-
-	@Autowired
-	public ProductreviewController(ProductService productService, ProductreviewService productreviewService) {
-		this.productService = productService;
-		this.productreviewService = productreviewService;
-	}
-
+	
 	@GetMapping("")
-	public String productreviewIndex(Model model) {
-		model.addAttribute("productreviews", delegate.showProductreviewList());
-		return "/product-reviews/index";
+	public String billofmaterialIndex(Model model) {
+		model.addAttribute("billofmaterials", delegate.showBilofmaterialList());
+		return "/billofmaterials/index";
 	}
 
 	@GetMapping("/edit/{id}")
-	public String editProductreview(Model model, @PathVariable("id") Integer id) {
-		model.addAttribute("productreview", delegate.getProductreview(id));
+	public String editBillofmaterial(Model model, @PathVariable("id") Integer id) {
+		model.addAttribute("billofmaterial", delegate.getBillofmaterial(id));
 		model.addAttribute("products", delegate.showProductList());
-		return "/product-reviews/edit";
+		return "/billofmaterials/edit";
 	}
 
 	@PostMapping("/edit/{id}")
 	public String postEditProductreview(
-			@ModelAttribute("productreview") @Validated(Add.class) Productreview productreview, BindingResult result,
+			@ModelAttribute("billofmaterial") @Validated(Add.class) Billofmaterial
+			billofmaterial, BindingResult result,
 			Model model, @PathVariable("id") Integer id,
 			@RequestParam(value = "action", required = true) String action) {
 
@@ -59,56 +49,55 @@ public class ProductreviewController {
 			if (result.hasErrors()) {
 				model.addAttribute("products", delegate.showProductList());
 
-				return "product-reviews/edit";
+				return "billofmaterials/edit";
 			}
-			productreview.setProduct(productreview.getProduct());
-			delegate.editProductreview(id, productreview);
+			billofmaterial.setProduct1(billofmaterial.getProduct1());
+			delegate.editBillofmaterial(id, billofmaterial);
 //			productreviewService.editProductreview(productreview, productreview.getProduct().getProductid());
 		}
-		return "redirect:/product-reviews";
+		return "redirect:/billofmaterials";
 	}
 
 	@GetMapping("/add")
-	public String addProductreview(Model model) {
-		model.addAttribute("productreview", new Productreview());
+	public String addbillofmaterial(Model model) {
+		model.addAttribute("billofmaterial", new Billofmaterial());
 		model.addAttribute("products", delegate.showProductList());
 
-		return "/product-reviews/add";
+		return "/billofmaterials/add";
 	}
 
 	@PostMapping("/add")
 	public String addProductreviewPost(
-			@ModelAttribute("productreview") @Validated(Add.class) Productreview productreview, BindingResult result,
+			@ModelAttribute("billofmaterial") @Validated(Add.class) Billofmaterial billofmaterial, BindingResult result,
 			@RequestParam(value = "action", required = true) String action, Model model) {
 
 		if (!action.equals("Cancel")) {
 			if (result.hasErrors()) {
 				model.addAttribute("products", delegate.showProductList());
-				return "/product-reviews/add";
+				return "/billofmaterials/add";
 			}
-			productreview.setProduct(productreview.getProduct());
-			delegate.addProductreview(productreview);
+			billofmaterial.setProduct1(billofmaterial.getProduct1());
+			delegate.addBillofmaterial(billofmaterial);
 //			productreviewService.saveProductreview(productreview, productreview.getProduct().getProductid());
 		}
-		return "redirect:/product-reviews";
+		return "redirect:/billofmaterials";
 	}
 
 	@GetMapping("/delete/{id}")
-	public String deleteProductreview(@PathVariable("id") Integer id, Model model) {
-		Productreview productreview = delegate.getProductreview(id);
-		delegate.deleteProductreview(productreview);
+	public String deleteBillofmaterial(@PathVariable("id") Integer id, Model model) {
+		Billofmaterial billofmaterial = delegate.getBillofmaterial(id);
+		delegate.deleteBillofmaterial(billofmaterial);
 //		productreviewService.delete(productreview);
-		model.addAttribute("productreviews", delegate.showProductreviewList());
-		return "redirect:/product-reviews";
+		model.addAttribute("billofmaterials", delegate.showBilofmaterialList());
+		return "redirect:/billofmaterials";
 	}
 
 	@GetMapping("/{id}")
 	public String getProduct(Model model, @PathVariable("id") Integer id) {
-		Productreview productreview = delegate.getProductreview(id);
+		Billofmaterial billofmaterial = delegate.getBillofmaterial(id);
 
-		model.addAttribute("productreview", productreview);
+		model.addAttribute("billofmaterial", billofmaterial);
 
-		return "product-reviews/information";
+		return "billofmaterials/information";
 	}
-
 }
